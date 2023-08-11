39999b5c9b41881b67e31fce24c21ccdc54d6eac
Diego Palacio <doyola@netgate.com>
2023-08-11 14:14:10 -0500

    Fixing tests

--- actions/calculate_test.go
+++ actions/calculate_test.go
@@ -24,10 +24,10 @@ func TestCalculateHandler(t *testing.T) {
 		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
 	}
 
-	expected := "Average Number of Customers in the System (L): 2.666667\n" +
-		"Average Number of Customers in the Queue (Lq): 1.333333\n" +
-		"Average Time a Customer Spends in the System (W): 1.166667\n" +
-		"Average Time a Customer Spends in the Queue (Wq): 0.583333\n"
+	expected := "Average Number of Customers in the System (L): 0.666737\n" +
+		"Average Number of Customers in the Queue (Lq): 0.000071\n" +
+		"Average Time a Customer Spends in the System (W): 0.333369\n" +
+		"Average Time a Customer Spends in the Queue (Wq): 0.000035\n"
 
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body:\n got:\n%s\n want:\n%s", rr.Body.String(), expected)
